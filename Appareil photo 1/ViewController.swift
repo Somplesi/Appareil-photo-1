0e549a72d6bf8dba96b0b07fae9e6574b80ce6e5
Rodolphe DUPUY <rodolphe.dupuy@free.fr>
2020-10-07 19:21:21 +0200

    Appareil Photo 1

--- Appareil photo 1/ViewController.swift
+++ Appareil photo 1/ViewController.swift
@@ -2,19 +2,82 @@
 //  ViewController.swift
 //  Appareil photo 1
 //
-//  Created by Matthieu PASSEREL on 30/07/2018.
-//  Copyright © 2018 Matthieu PASSEREL. All rights reserved.
+//  Created by Rodolphe DUPUY on 07/10/2020.
+//  Copyright © 2020 Rodolphe DUPUY. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageViewChoisie: UIImageView!
+    @IBOutlet weak var noImageLabel: UILabel!
+    
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if imageViewChoisie.image == nil {
+            noImageLabel.isHidden = false
+        } else {
+            noImageLabel.isHidden = true
+        }
+    }
+    
+    func presentWithSource(_ source: UIImagePickerController.SourceType)  {
+        imagePicker.sourceType = source
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func prendrePhoto(_ sender: UIButton) {
+        let alerteActionSheet = UIAlertController(title: "Prendre uns photo", message: "Choisissez le média", preferredStyle: .actionSheet)
+        let camera = UIAlertAction(title: "Appareil photo", style: .default) { (action) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                self.presentWithSource(.camera)
+            } else {
+                let alerte = UIAlertController(title: "Erreur", message: "Aucun appareil photo n'est disponible", preferredStyle: .alert)
+                let annuler = UIAlertAction(title: "Je comprends", style: .destructive, handler: nil)
+                alerte.addAction(annuler)
+                self.present(alerte, animated: true, completion: nil)
+            }
+        }
+        let gallery = UIAlertAction(title: "Gallerie de photos", style: .default) { (action) in
+            self.presentWithSource(.photoLibrary)
+        }
+        let cancel = UIAlertAction(title: "Annuler", style: .cancel, handler: nil)
+        alerteActionSheet.addAction(camera)
+        alerteActionSheet.addAction(gallery)
+        alerteActionSheet.addAction(cancel)
+        
+        if let popover = alerteActionSheet.popoverPresentationController {
+            popover.sourceView = view
+            popover.sourceRect = CGRect(x: view.frame.midX, y: view.frame.midY, width: 0, height: 0)
+            popover.permittedArrowDirections = []
+        }
+        present(alerteActionSheet, animated: true, completion: nil)
+    }
 }
 
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let edite = info[.editedImage] as? UIImage {
+            imageViewChoisie.image = edite
+        } else if let originale = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageViewChoisie.image = originale
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
